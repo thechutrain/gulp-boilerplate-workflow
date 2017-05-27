2f09c53488b07a30b14b9602f53872b2cf95f4c6
thechutrain <chu.chuyalan@gmail.com>
2017-05-26 22:50:07 -0400

    basic index page and gulpfile with default task

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,6 @@ var gulp = require('gulp');
 
 gulp.task('default', function(){
   console.log('hey yo Im working in the default task')
+  // var src = gulp.src('/public')
+  // console.log(src.pipe);
 })
