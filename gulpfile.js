efabe189eeb34d3f62b7a568727419d4e9f6505d
thechutrain <chu.chuyalan@gmail.com>
2017-05-29 14:11:22 -0400

    no gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,10 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 // var uglify = require('gulp-uglify')
 var cleanCSS = require('gulp-clean-css')
-const inject = require('gulp-inject')
 
 // PATHS
 const CSS_URL = 'development/static/css/**/*.css'
 const DIST_URL = 'dist/static/'
-
-// HTML task
-gulp.task('html', function () {
-	const distSources = gulp.src(['dist/static/styles.css', 'dist/static/bundle.js'], {read: false})
-	return gulp.src('./dist/*.html')
-		.pipe(inject(distSources, { ignorePath: '/dist', addRootSlash: false } ))
-		.pipe(gulp.dest('./dist'))
-})
 
 // Css tasks
 gulp.task('css', function(){
