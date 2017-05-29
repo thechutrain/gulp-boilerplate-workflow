4f58f5261c2503b5dcb73a9b52321d4732033b35
thechutrain <chu.chuyalan@gmail.com>
2017-05-29 15:02:21 -0400

    build task, will concat all css & js files, and inject into my dist/ html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 'use strict'
 var gulp = require('gulp')
 var concat = require('gulp-concat')
-// var uglify = require('gulp-uglify')
+var uglify = require('gulp-uglify')
 var cleanCSS = require('gulp-clean-css')
+var useref = require('gulp-useref')
+var gulpif = require('gulp-if')
 
 // PATHS
 const CSS_URL = 'development/static/css/**/*.css'
 const DIST_URL = 'dist/static/'
+
+// html
+gulp.task('build', function(){
+	return gulp.src('development/index.html')
+		.pipe(useref())
+		.pipe(gulpif('*.css', cleanCSS({compatibility: 'ie8'})))
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulp.dest('dist/'))
+		// .pipe(gulpif('*.css', ))
+		// .pipe(gulp.dest(DIST_URL))
+})
 
 // Css tasks
 gulp.task('css', function(){
