a48a1115f88a7d4ebf60d705ad8175e1d929bbb2
thechutrain <chu.chuyalan@gmail.com>
2017-05-29 16:07:13 -0400

    go rid of console errors

--- development/static/js/test1.js
+++ development/static/js/test1.js
@@ -1,1 +1,11 @@
 alert('hello there!!! Check out the console')
+
+class Person {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+}
+
+const jon = new Person('jon', 77)
+console.log(typeof jon)
