3029f8613376c9438787397b974981c35fb30cf2
thechutrain <chu.chuyalan@gmail.com>
2017-05-29 12:58:21 -0400

    added linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
+'use strict'
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+// var uglify = require('gulp-uglify')
+var cleanCSS = require('gulp-clean-css')
+// const inject = require('gulp-inject')
 
 // PATHS
-var CSS_URL = 'development/static/css/**/*.css';
-var DIST_URL = 'dist/static/';
+const CSS_URL = 'development/static/css/**/*.css'
+const DIST_URL = 'dist/static/'
 
 // Css tasks
 gulp.task('css', function(){
-  console.log('Loading css tasks ...');
-  return gulp.src(['development/static/css/normalize_reset.css', CSS_URL])
+	// console.log('Loading css tasks ...')
+	return gulp.src(['development/static/css/normalize_reset.css', CSS_URL])
     .pipe(concat('styles.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest(DIST_URL))
@@ -18,5 +20,5 @@ gulp.task('css', function(){
 
 
 gulp.task('default', function(){
-  console.log('hey yo Im working in the default task')
+	// console.log('hey yo Im working in the default task')
 })
